da8a1947e07abfe9a515e40f06677afa972053e1
Vishnupriya <priya.singh9349@gmail.com>
2023-12-13 20:04:04 +0530

    Added code for JDBC5

--- employee-payroll/src/main/java/com/day15/PayrollDBService.java
+++ employee-payroll/src/main/java/com/day15/PayrollDBService.java
@@ -12,7 +12,7 @@ public class PayrollDBService {
     public PayrollDBService() {
         String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false&allowPublicKeyRetrieval=true";
         String username = "root";
-        String password = "Priya@002";
+        String password = "anubhav08";
 
         try {
             this.connection = DriverManager.getConnection(jdbcUrl, username, password);
@@ -50,5 +50,36 @@ public class PayrollDBService {
             }
         }
         return null;
+    }
+
+    // UC5
+    public List<EmpPayroll> getEmployeesJoinedInRange(Date startDate, Date endDate) throws SQLException {
+        List<EmpPayroll> employeeList = new ArrayList<>();
+        String query = "SELECT * FROM employee_payroll WHERE Start BETWEEN ? AND ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setDate(1, new java.sql.Date(startDate.getTime()));
+            preparedStatement.setDate(2, new java.sql.Date(endDate.getTime()));
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                EmpPayroll employee = new EmpPayroll(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("gender"),
+                        resultSet.getDouble("salary"),
+                        resultSet.getDate("Start"),
+                        resultSet.getString("phone"),
+                        resultSet.getString("address"),
+                        resultSet.getString("department"),
+                        resultSet.getDouble("basicPay"),
+                        resultSet.getDouble("deductions"),
+                        resultSet.getDouble("taxablePay"),
+                        resultSet.getDouble("incomeTax"),
+                        resultSet.getDouble("netPay"));
+                employeeList.add(employee);
+            }
+        }
+        return employeeList;
     }
 }
