16c2bee1b235f442e55ff33595a7481ecfa0b506
Vishnupriya <priya.singh9349@gmail.com>
2023-12-13 23:00:46 +0530

    Added code for JDBC10 for UC2-UC7

--- employee-payroll/src/main/java/com/day15/EPService.java
+++ employee-payroll/src/main/java/com/day15/EPService.java
@@ -1,16 +1,20 @@
 package com.day15;
 
 import java.sql.SQLException;
-import java.sql.Date;
+import java.util.Map;
 
 public class EPService {
     public static void main(String[] args) {
-        PayrollDBService payrollDBService = PayrollDBService.getInstance();
+        PayrollDBService service = PayrollDBService.getInstance();
         try {
-            EmpPayroll newEmployee = payrollDBService.addEmployeeWithPayrollDetails(79, "Rishab", 12000, new Date(2023-12-13), "M", "1234567890", "Address 79", "QA", 5000, 1200, 7000, 700, 9000);
-            System.out.println("New Employee Added: " + newEmployee.getName());
+            Map<String, Map<String, Double>> stats = service.getSalaryStatsByGender();
+            stats.forEach((gender, statistics) -> {
+                System.out.println("Gender: " + gender);
+                statistics.forEach((key, value) -> System.out.println(key + ": " + value));
+            });
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
 }
