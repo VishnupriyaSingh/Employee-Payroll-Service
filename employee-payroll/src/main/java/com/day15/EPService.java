246f8d0c3fcffbf79b85469f69fb6c70853c3d9d
Vishnupriya <priya.singh9349@gmail.com>
2023-12-13 19:01:18 +0530

    Added code for JDBC-3

--- employee-payroll/src/main/java/com/day15/EPService.java
+++ employee-payroll/src/main/java/com/day15/EPService.java
@@ -1,52 +1,69 @@
 package com.day15;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class EPService {
 
     public static void main(String[] args) {
         try {
-            List<EmpPayroll> payrollList = getEmpPayrollData();
+            updateEmployeeSalary("Terissa", 3000000.00);
+            EmpPayroll updatedEmployee = getEmployeeDataByName("Terissa");
         } catch (PayrollServiceException e) {
             System.err.println("Error: " + e.getMessage());
         }
     }
 
-    public static List<EmpPayroll> getEmpPayrollData() throws PayrollServiceException {
+    public static void updateEmployeeSalary(String name, double newSalary) throws PayrollServiceException {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service";
+        String username = "root";
+        String password = "Priya@002";
+
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+            String updateQuery = "UPDATE employee_payroll SET Salary = ? WHERE Name = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
+                preparedStatement.setDouble(1, newSalary);
+                preparedStatement.setString(2, name);
+
+                System.out.println("Salary updated for " + name);
+            }
+        } catch (SQLException e) {
+            throw new PayrollServiceException("Failed to update salary in database");
+        }
+    }
+
+    public static EmpPayroll getEmployeeDataByName(String name) throws PayrollServiceException {
         String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service";
         String username = "root";
         String password = "anubhav08";
 
-        List<EmpPayroll> payrollList = new ArrayList<>();
-
         try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
-            String query = "SELECT * FROM employee_payroll";
-            try (Statement statement = connection.createStatement()) {
-                ResultSet resultSet = statement.executeQuery(query);
+            String query = "SELECT * FROM employee_payroll WHERE Name = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setString(1, name);
+                ResultSet resultSet = preparedStatement.executeQuery();
 
-                while (resultSet.next()) {
+                if (resultSet.next()) {
                     int id = resultSet.getInt("ID");
-                    String name = resultSet.getString("Name");
+                    String employeeName = resultSet.getString("Name");
                     double salary = resultSet.getDouble("Salary");
                     Date start = resultSet.getDate("Start");
 
                     EmpPayroll employee = new EmpPayroll();
                     employee.setId(id);
-                    employee.setName(name);
+                    employee.setName(employeeName);
                     employee.setSalary(salary);
                     employee.setStart(start);
 
-                    payrollList.add(employee);
-
                     System.out.println("ID: " + id + ", Name: " + name + ", Salary: " + salary + ", Start Date: " + start);
+
+                    return employee;
+                } else {
+                    String msg = "Employee not found with name: " + name;
+                    throw new PayrollServiceException(msg);
                 }
             }
         } catch (SQLException e) {
-            throw new PayrollServiceException("Failed to retrieve payroll data", e);
+            throw new PayrollServiceException("Database error occurred while retrieving employee data");
         }
-
-        return payrollList;
     }
 }
