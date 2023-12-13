dabc2554086a8a02e1dd2566a68e94e888f4f7ff
Vishnupriya <priya.singh9349@gmail.com>
2023-12-13 22:43:08 +0530

    Added code for JDBC8

--- employee-payroll/src/main/java/com/day15/EPService.java
+++ employee-payroll/src/main/java/com/day15/EPService.java
@@ -7,7 +7,7 @@ public class EPService {
     public static void main(String[] args) {
         PayrollDBService payrollDBService = PayrollDBService.getInstance();
         try {
-            EmpPayroll newEmployee = payrollDBService.addEmployeeToPayroll(78, "Arnav", 12000, new Date(2023-12-12), "M", "1234567890", "Address 78", "QA", 5000, 1200, 7000, 700, 9000);
+            EmpPayroll newEmployee = payrollDBService.addEmployeeWithPayrollDetails(79, "Rishab", 12000, new Date(2023-12-13), "M", "1234567890", "Address 79", "QA", 5000, 1200, 7000, 700, 9000);
             System.out.println("New Employee Added: " + newEmployee.getName());
         } catch (SQLException e) {
             e.printStackTrace();
