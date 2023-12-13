586417d6ae44b2d57bee4a2f7930d74ff6953599
Vishnupriya <priya.singh9349@gmail.com>
2023-12-13 12:56:20 +0530

    Added code for JDBC-2

--- employee-payroll/src/main/java/com/day15/EPService.java
+++ employee-payroll/src/main/java/com/day15/EPService.java
@@ -1,32 +1,52 @@
 package com.day15;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EPService {
 
     public static void main(String[] args) {
+        try {
+            List<EmpPayroll> payrollList = getEmpPayrollData();
+        } catch (PayrollServiceException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static List<EmpPayroll> getEmpPayrollData() throws PayrollServiceException {
         String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service";
         String username = "root";
-        String password = "Priya@002";
+        String password = "anubhav08";
 
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("MySQL JDBC Driver Registered!");
+        List<EmpPayroll> payrollList = new ArrayList<>();
 
-            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+            String query = "SELECT * FROM employee_payroll";
+            try (Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery(query);
 
-            if (connection != null) {
-                System.out.println("Successfully connected to MySQL database.");
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("ID");
+                    String name = resultSet.getString("Name");
+                    double salary = resultSet.getDouble("Salary");
+                    Date start = resultSet.getDate("Start");
+
+                    EmpPayroll employee = new EmpPayroll();
+                    employee.setId(id);
+                    employee.setName(name);
+                    employee.setSalary(salary);
+                    employee.setStart(start);
+
+                    payrollList.add(employee);
+
+                    System.out.println("ID: " + id + ", Name: " + name + ", Salary: " + salary + ", Start Date: " + start);
+                }
             }
-
-        } catch (ClassNotFoundException e) {
-            System.out.println("MySQL JDBC Driver not found.");
-            e.printStackTrace();
         } catch (SQLException e) {
-            System.out.println("Connection to MySQL database failed.");
-            e.printStackTrace();
+            throw new PayrollServiceException("Failed to retrieve payroll data", e);
         }
+
+        return payrollList;
     }
 }
---
+++ employee-payroll/src/main/java/com/day15/EmpPayroll.java
@@ -0,0 +1,101 @@
+package com.day15; 
+import java.util.Date;
+import java.util.List;
+
+public class EmpPayroll {
+    private int id;
+    private String name;
+    private String gender;
+    private double salary;
+    private Date startDate;
+    private String phone;
+    private String address;
+    private String department;
+    private double basicPay;
+    private double deductions;
+    private double taxablePay;
+    private double incomeTax;
+    private double netPay;
+    private List<String> departments; //UC9
+
+    public EmpPayroll()
+    {
+
+    }
+
+    public EmpPayroll(int id, String name, String gender, double salary, Date startDate, String phone, String address, String department, double basicPay, double deductions, double taxablePay, double incomeTax, double netPay) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.salary = salary;
+        this.startDate = startDate;
+        this.phone = phone;
+        this.address = address;
+        this.department = department;
+        this.basicPay = basicPay;
+        this.deductions = deductions;
+        this.taxablePay = taxablePay;
+        this.incomeTax = incomeTax;
+        this.netPay = netPay;
+    }
+
+    @Override
+    public String toString() {
+        return "EmpPayroll{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", startDate=" + startDate +
+                ", phone=" + phone +
+                ", address=" + address +
+                ", department=" + department +
+                ", basicPay=" + basicPay +
+                ", deductions=" + deductions +
+                ", taxablePay=" + taxablePay +
+                ", incomeTax=" + incomeTax +
+                ", netPay=" + netPay +
+                '}';
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setStart(java.sql.Date start) {
+        this.startDate = start;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getStart() {
+        return startDate;
+    }
+
+    public List<String> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(List<String> departments) {
+        this.departments = departments;
+    }
+
+    public void addDepartment(String department) {
+        this.departments.add(department);
+    }
+}
