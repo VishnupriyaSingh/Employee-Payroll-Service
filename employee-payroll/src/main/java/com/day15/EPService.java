251bfad5036717e2e64fd12a629d1841a292c99e
Vishnupriya <priya.singh9349@gmail.com>
2023-12-13 19:48:03 +0530

    Added code for Refactor4

--- employee-payroll/src/main/java/com/day15/EPService.java
+++ employee-payroll/src/main/java/com/day15/EPService.java
@@ -1,52 +1,20 @@
 package com.day15;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.SQLException;
 
 public class EPService {
 
     public static void main(String[] args) {
         try {
-            List<EmpPayroll> payrollList = getEmpPayrollData();
-        } catch (PayrollServiceException e) {
-            System.err.println("Error: " + e.getMessage());
-        }
-    }
-
-    public static List<EmpPayroll> getEmpPayrollData() throws PayrollServiceException {
-        String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service";
-        String username = "root";
-        String password = "Priya@002";
-
-        List<EmpPayroll> payrollList = new ArrayList<>();
-
-        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
-            String query = "SELECT * FROM employee_payroll";
-            try (Statement statement = connection.createStatement()) {
-                ResultSet resultSet = statement.executeQuery(query);
-
-                while (resultSet.next()) {
-                    int id = resultSet.getInt("ID");
-                    String name = resultSet.getString("Name");
-                    double salary = resultSet.getDouble("Salary");
-                    Date start = resultSet.getDate("Start");
-
-                    EmpPayroll employee = new EmpPayroll();
-                    employee.setId(id);
-                    employee.setName(name);
-                    employee.setSalary(salary);
-                    employee.setStart(start);
-
-                    payrollList.add(employee);
-
-                    System.out.println("ID: " + id + ", Name: " + name + ", Salary: " + salary + ", Start Date: " + start);
-                }
+            PayrollDBService payrollDBService = PayrollDBService.getInstance();
+            EmpPayroll employee = payrollDBService.getEmployeeDataByName("Terissa");
+            if (employee != null) {
+                System.out.println("Found employee: " + employee.getName());
+            } else {
+                System.out.println("Employee not found.");
             }
         } catch (SQLException e) {
-            throw new PayrollServiceException("Failed to retrieve payroll data", e);
+            e.printStackTrace();
         }
-
-        return payrollList;
     }
 }
